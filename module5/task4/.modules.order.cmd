defd0867121e475dd0f59da1bdeab6eba41eae5f
Your Name <you@example.com>
2026-05-17 18:35:22 +0700

    Remove compiled files from repository

--- module5/task4/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/slarkfish/Eltex/module5/task4/modules.order := {   echo /home/slarkfish/Eltex/module5/task4/char_dev_module.o; :; } > /home/slarkfish/Eltex/module5/task4/modules.order
--- module5/task4/modules.order
+++
@@ -1,1 +0,0 @@
-/home/slarkfish/Eltex/module5/task4/char_dev_module.o
